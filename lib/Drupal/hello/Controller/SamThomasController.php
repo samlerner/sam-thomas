356c792c1bdd12d867ddad14a7846606058b6c4b
Thomas Howell, Acquia Support <thomas.howell@acquia.com>
2014-05-01 13:57:23 -0500

    Added files for hello world version.

---
+++ lib/Drupal/hello/Controller/SamThomasController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\samthomas\Controller\SamThomasController.
+ */
+
+namespace Drupal\samthomas\Controller
+
+/**
+ * Controller routines for hello page routes.
+ */
+class SamThomasController {
+  public function description() {
+    return array(
+      '#markup' => t('<p>Test!</p>'),
+    );
+  }
+}
