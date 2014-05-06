8fa634ff1509df7706be38f551a5ba9985613690
Sam Lerner <sam.lerner@acquia.com>
2014-05-06 15:47:31 -0400

    Adding new About Us page

--- lib/Drupal/samthomas/Controller/SamThomasController.php
+++ lib/Drupal/samthomas/Controller/SamThomasController.php
@@ -11,9 +11,17 @@ namespace Drupal\samthomas\Controller;
  * Controller routines for hello page routes.
  */
 class SamThomasController {
+
   public function description() {
     return array(
       '#markup' => t('<p>Test!</p>'),
     );
   }
+
+  public function about() {
+  	return array(
+  		'#markup' => t('<p>This is our about page.</p>'),
+  	);
+  }
+
 }
